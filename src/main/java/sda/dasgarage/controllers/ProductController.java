436610be6f77f50a762cf7b4b9d868b84374ad6a
DanielStoicaRO <dmdprofesional@gmail.com>
2021-09-06 20:32:37 +0300

    add method: add edit delete

--- src/main/java/sda/dasgarage/controllers/ProductController.java
+++ src/main/java/sda/dasgarage/controllers/ProductController.java
@@ -2,26 +2,41 @@ package sda.dasgarage.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import sda.dasgarage.entities.ProductEntity;
 import sda.dasgarage.repositories.ProductRepository;
 
 @Controller
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     public ProductController() {
         System.out.println(getClass().getSimpleName() + " created");
     }
-
-    @Autowired
-    private ProductRepository productRepository;
 
     @GetMapping("/frontpage")
     public ModelAndView getFrontPage() {
          ModelAndView modelAndView = new ModelAndView("frontpage");
          modelAndView.addObject("stockList", productRepository.findAll());
          return modelAndView;
+    }
+
+    @GetMapping("/product/add")
+    public ModelAndView productAdd() {
+        ModelAndView modelAndView = new ModelAndView("productAdd");
+        modelAndView.addObject("product", new ProductEntity());
+        return modelAndView;
+    }
+
+    @PostMapping("/product/save")
+    public ModelAndView productSave(@ModelAttribute("product") ProductEntity productEntity) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/frontpage");
+        productRepository.save(productEntity);
+        return modelAndView;
     }
 
     @GetMapping("/product/view/{id}")
@@ -31,6 +46,20 @@ public class ProductController {
         return modelAndView;
     }
 
+    @GetMapping("/product/edit/{id}")
+    public ModelAndView productEdit(@PathVariable Integer id){
+        ModelAndView modelAndView = new ModelAndView("productEdit");
+        modelAndView.addObject("product", productRepository.getById(id));
+        return modelAndView;
+    }
+
+    @GetMapping("/product/delete/{id}")
+    public ModelAndView productDelete(@PathVariable Integer id){
+        ProductEntity productEntity = productRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("No product"));
+        productRepository.delete(productEntity);
+        ModelAndView modelAndView = new ModelAndView("redirect:/frontpage");
+        return modelAndView;
+    }
 
 
 }
